4f665319db54e95a6eb6019840faf40c2c3b7273
Gedeon Mg <gedeonmuhawenayo@gmail.com>
2020-01-24 23:41:06 +0000

    linked list

--- linkedLists.py
+++ linkedLists.py
@@ -18,10 +18,12 @@ class myList():
         cur_pos.next = node
     def printlist(self):
         cur_pos = self.head
+        l = []
         while cur_pos.next != None:
-            print(cur_pos.data)
+            l.append(cur_pos.data)
             cur_pos = cur_pos.next
-        print(cur_pos.data)
+        l.append(cur_pos.data)
+        return l
     def insert(self,pos,data):
         k = 1
         node = Node(data)
@@ -41,6 +43,13 @@ class myList():
         hook = cur_pos.next
         hook2 = hook.next
         cur_pos.next = hook2
+    def lenght(self):
+        l = 1
+        cur_pos = self.head
+        while cur_pos.next != None:
+            cur_pos = cur_pos.next
+            l += 1
+        return l
 
 l = myList()
 l.add(3)
@@ -50,9 +59,15 @@ l.add(3)
 l.add(5)
 l.add(1)
 l.add(4)
-l.printlist()
+print(l.printlist())
+print(l.lenght())
 l.insert(2,400)
-l.printlist()
+print(l.printlist())
+print(l.lenght())
 l.delete(2)
-l.printlist()
+print(l.printlist())
+print(l.lenght())
+l.delete(2)
+print(l.printlist())
+print(l.lenght())
     
