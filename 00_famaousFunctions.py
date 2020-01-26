019e7511ad7dab3af80d3165122575bab72227f7
Gedeon Mg <gedeonmuhawenayo@gmail.com>
2020-01-26 18:06:47 +0000

    The most used functions

--- 00_famaousFunctions.py
+++ 00_famaousFunctions.py
@@ -11,3 +11,20 @@ def possible_div(n):
         i += 1
     return l
 print(possible_div(10))  
+
+## inplace removing element from a list of posivite numbers
+def remove_number(arr,num):
+    k = 0
+    print(arr)
+    for i in range(len(arr)):
+        if arr[i]==num: 
+            arr[i]=-1
+            k+=1
+    arr.sort(reverse=True)
+    while k>0:
+        k-=1
+        arr.pop()
+    return arr
+print(remove_number([3,3,2,33],3))
+    
+
