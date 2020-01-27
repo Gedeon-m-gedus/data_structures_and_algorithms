a7c9216c8dfd1107198b3e71ac5b4cd7f9b0d432
Gedeon Mg <gedeonmuhawenayo@gmail.com>
2020-01-27 12:32:33 +0000

    Stacks

--- 02_stacks.py
+++ 02_stacks.py
@@ -12,6 +12,7 @@ class myStack():
         node.next = self.head
         self.head=node
     def printStack(self):
+        if self.head == None: return
         cur_pos = self.head
         l = []
         while cur_pos.next != None:
@@ -20,6 +21,7 @@ class myStack():
         l.append(cur_pos.data)
         return l
     def pop(self):
+        if self.head == None: return
         d = self.head.data
         self.head = self.head.next
         return d
@@ -29,5 +31,11 @@ m.push(0)
 m.push(9)
 m.push(19)
 print(m.printStack())
+print(m.pop())
+print(m.printStack())
+print(m.pop())
+print(m.pop())
+print(m.pop())
+print(m.pop())
 print(m.pop())
 print(m.printStack())
