98502524a6db7181b6ae237de846348d2716fa03
Gedeon Mg <gedeonmuhawenayo@gmail.com>
2020-01-30 12:16:41 +0000

    Binary search using recursion

--- 04_binarySearch.py
+++ 04_binarySearch.py
@@ -1,30 +1,16 @@
-def binary_search(input_array, value):
-    """Your code goes here."""
-    l = len(input_array)
-    pos = l//2
-    i = 0
-    for j in range(l):
-        indx = pos
-        print (j,' ',input_array[indx],' ',indx)
-        i+=1
-        
-        if input_array[indx]==value:
-            return 1
-        elif input_array[indx]<value: 
-            b = l//(4*i)
-            if b == 0:b=1
-            pos = pos + b
-            if indx == pos: return -1
-        elif input_array[indx]>value: 
-            b = l//(4*i)
-            if b == 0:b=1
-            pos = pos - b
-            if indx == pos: return -1
-        if pos > l-1 or pos < 0:return -1
-        
+def binarySearch(arr , number):
+    length = len(arr)
+    midPoint = length//2
+    if length == 0 or midPoint==0: return False
+    if arr[midPoint]==number: return True
+    elif arr[midPoint]>number:
+        return binarySearch(arr[0:midPoint] , number)
+    else:# arr[midPoint]<number:
+        return binarySearch(arr[midPoint:] , number)
 
-test_list = [1,3,9,11,15,19,29,39,41,71,92]
-test_val1 = 2
-test_val2 = 92
-print (binary_search(test_list, test_val1))
-print (binary_search(test_list, test_val2))
+arr = [1,2,3,5,6,7,8,9]
+number = 20
+print(binarySearch(arr , number))
+arr = [1,2,3,5,6,7,8,9]
+number = 8
+print(binarySearch(arr , number))
