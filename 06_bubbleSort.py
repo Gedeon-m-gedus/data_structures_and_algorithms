6170ab57227a25aa3432a2bf2a280b67311d979e
Gedeon Mg <gedeonmuhawenayo@gmail.com>
2020-01-30 13:33:30 +0000

    Bubble sort

--- 06_bubbleSort.py
+++ 06_bubbleSort.py
@@ -1,3 +1,4 @@
+## this bubble sort algorithms, at each iteration it bubbles the big number to the right side
 def bubbleSort(arr):
     for n in range(len(arr)):
         j=0
@@ -6,3 +7,6 @@ def bubbleSort(arr):
                 arr[i],arr[j]=arr[j],arr[i]
             j+=1
     return arr
+
+arr = [2,3,4,0,7,0,3,1]
+print(bubbleSort(arr))
