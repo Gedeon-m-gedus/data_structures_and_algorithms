9fbd615b2ab3c7955bda66de8d74b58e9aed8030
Gedeon Mg <gedeonmuhawenayo@gmail.com>
2020-03-03 20:35:27 +0000

    binary tree

--- 07_binary_tree.py
+++ 07_binary_tree.py
@@ -3,3 +3,18 @@ class Node():
         self.data = data
         self.left = None
         self.right = None
+    def add(self,data):
+        if self.data:
+            if self.data < data:
+                if self.left is not None:
+                    self.left.add(data):
+                else:
+                    self.left = Node(data)
+            else:
+                if self.right is not None:
+                    self.right.add(data)
+                else:
+                    self.right = Node(data)
+
+        else:
+            self.data = Node(data)
